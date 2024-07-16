cb14dc27f1c68a874c860858a4cd32868d20d1c5
Ben Thomasson <bthomass@redhat.com>
2024-07-16 10:39:23 -0400

    Add coverage

--- djcoverage/__init__.py
+++ djcoverage/__init__.py
@@ -1,14 +1,15 @@
 
+import coverage
+cov = coverage.Coverage()
+
 
 def coverage_middleware(get_response):
 
     def middleware(request):
-
+        cov.start()
         response = get_response(request)
-
-        print('woo')
-
+        cov.stop()
+        cov.save()
         return response
-
 
     return middleware
